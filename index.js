d553c3dcc24770e1c71b6a93670339311ac25c93
Steven Jankowski <steven.jankowski1@gmail.com>
2021-12-15 18:49:24 +0000

    commit tree 5d571d50627dd27ade6e59f0bdcc3c3705030976

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code your solution here
+const record = [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "W"},
+    {year: "2016", result: "N/A"}
+  ]
+
+  function superbowlWin(record){
+    let outcome = record.find(function(outcomes) {
+      return outcomes.result === 'W';
+    })
+    return outcome ? outcome.year : undefined
+}
+
+
+
+
+
+
